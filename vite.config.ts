d2f98a33bf77f2e6c5b751bad80b9b32fe2a8e95
Plarget <67307148+Plarget@users.noreply.github.com>
2025-09-10 23:30:17 +0400

    test legacy

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import * as path from "path"
-import { createSvgIconsPlugin } from "vite-plugin-svg-icons"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
+import * as path from "path";
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 
 export default defineConfig({
-  plugins: [react(), createSvgIconsPlugin({
-    iconDirs: [path.resolve(process.cwd(), "src/shared/assets/images/icons")],
-    symbolId: "icon-[dir]-[name]",
-    inject: "body-last",
-    customDomId: "__svg__icons__dom__",
-  })],
+  plugins: [
+    react(),
+    legacy({
+      targets: ["defaults", "chrome >= 60"],
+    }),
+    createSvgIconsPlugin({
+      iconDirs: [path.resolve(process.cwd(), "src/shared/assets/images/icons")],
+      symbolId: "icon-[dir]-[name]",
+      inject: "body-last",
+      customDomId: "__svg__icons__dom__",
+    }),
+  ],
   resolve: {
-    alias: [
-      {find: "@", replacement: path.resolve(__dirname, "src")}
-    ]
-  }
-})
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+});
